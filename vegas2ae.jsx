05b7ed95fe0a980685810538739f0d36fa91e57a
neverlink <29165285+neverlink@users.noreply.github.com>
2024-08-20 22:53:50 +0000

    refactor: un-nest methods, etc

--- vegas2ae.jsx
+++ vegas2ae.jsx
@@ -1,3 +1,180 @@
+function getKeyframeEase(curveType) {
+    var easeIn;
+    var easeOut;
+
+    if (curveType == 1) { // Linear
+        return null;
+    } else if (curveType == -2) { // Slow in, fast out
+        easeIn = new KeyframeEase(0, 75);
+        easeOut = new KeyframeEase(0, 0.1);
+    } else if (curveType == 2) { // Fast in, slow out
+        easeIn = new KeyframeEase(0, 0.1);
+        easeOut = new KeyframeEase(0, 75);
+    } else if (curveType == 4) { // Easy Ease
+        easeIn = new KeyframeEase(0, 33.33);
+        easeOut = easeIn;
+    } else if (curveType == -4) { // Inverted Easy Ease                    
+        easeIn = new KeyframeEase(100, 5);
+        easeOut = easeIn;
+    } else {
+        alert('Unregistered CurveType: ' + curveType);
+    }
+
+    return easeIn && easeOut ? [easeIn, easeOut] : null;
+}
+
+function applyFades(clip, layer, propHandle, markKeyframes) {
+    var minValue = 0;
+    var maxValue = 100;
+
+    if (clip.FadeTimeIn > 0) {
+        var fadeStart = layer.inPoint;
+        var fadeEnd = fadeStart + (clip.FadeTimeIn);
+        propHandle.setValueAtTime(fadeStart, minValue);
+        propHandle.setValueAtTime(fadeEnd, maxValue);
+        
+        var easeArgs = getKeyframeEase(clip.CurveIn);
+        if (easeArgs) {
+            propHandle.setTemporalEaseAtKey(1, [easeArgs[0]]);
+            propHandle.setTemporalEaseAtKey(2, [easeArgs[1]]);
+        }
+
+        if (markKeyframes) { 
+            var marker = new MarkerValue('Fade In'); 
+            marker.duration = fadeEnd - fadeStart;
+            layer.marker.setValueAtTime(fadeStart, marker);
+        }
+    }
+
+    if (clip.FadeTimeOut > 0) {
+        var fadeStart = layer.outPoint - (clip.FadeTimeOut);
+        var fadeEnd = layer.outPoint;
+        
+        propHandle.setValueAtTime(fadeStart, maxValue);
+        propHandle.setValueAtTime(fadeEnd, minValue);
+
+        var easeArgs = getKeyframeEase(clip.CurveOut);
+        if (easeArgs) {
+            propHandle.setTemporalEaseAtKey(propHandle.numKeys - 1, [easeArgs[0]]);
+            propHandle.setTemporalEaseAtKey(propHandle.numKeys, [easeArgs[1]]);
+        };
+
+        if (markKeyframes) {
+            var marker = new MarkerValue('Fade In');
+            marker.duration = fadeEnd - fadeStart;
+            layer.marker.setValueAtTime(fadeStart, marker);
+        }
+    }
+}
+
+function getPlaceholder(edlClip, options) {
+    return app.project.importPlaceholder(
+        edlClip.FileName,
+        options['compWidth'],
+        options['compHeight'],
+        options['compFrameRate'],
+        edlClip.Length
+    )
+}
+
+function getSolid(edlClip, options) {
+    edlClip.FileName = 'Solid';
+    var footageItem = getPlaceholder(edlClip, options);
+    
+    function hexToFloats(hex) {
+        hex = hex.replace(/^#/, '');
+        if (!(hex.length !== 3 || hex.length !== 6))
+            alert('Invalid hex code!');
+        if (hex.length === 3) // Expand shorthand (e.g., #abc -> #aabbcc)
+          hex = hex.split('').map(function(c) {return c + c}).join('');
+        var r = parseInt(hex.substring(0, 2), 16) / 255;
+        var g = parseInt(hex.substring(2, 4), 16) / 255;
+        var b = parseInt(hex.substring(4, 6), 16) / 255;
+        return [r, g, b];
+    }
+    
+    footageItem.replaceWithSolid(
+        hexToFloats(options['solidColorHex']),
+        'Solid',
+        options['compWidth'],
+        options['compHeight'],
+        options['compPixelAspect']
+    );
+    
+    return footageItem;
+}
+
+function findFootageItem(edlClip, clipFile) {
+    // Project items are indexed from 1
+    for (var j = 1; j <= app.project.numItems; j++) {
+        var projectItem = app.project.items[j];
+        // Solids have no FileName
+        if (!edlClip.FileName && projectItem instanceof SolidSource)
+            return projectItem;
+        else if (projectItem.file && projectItem.file.fullName === clipFile.fullName)
+            return projectItem;
+    }
+}
+
+function importFootage(edlClip, options) {
+    var clipFile = File(edlClip.FileName);
+    var footageItem = findFootageItem(edlClip, clipFile);
+
+    if (footageItem)
+        return footageItem;
+    if (!edlClip.FileName)
+        return getSolid(edlClip, options);
+    if (clipFile.exists)
+        return app.project.importFile(new ImportOptions(clipFile));
+    if (!options['ignoreFailedImports']) {
+        options['ignoreFailedImports'] = Window.confirm(
+            'Failed to import file:\n' + edlClip.FileName +
+            '\nA placeholder will be used instead.' +
+            '\n\nPress OK to suppress this warning.'
+        );
+    }
+    return getPlaceholder(edlClip, options);
+}
+
+function parseEDL(edlFile) {
+    edlFile.open('r');
+    var lines = edlFile.read().split('\n');
+    edlFile.close();
+
+    // Remove last row (expected to be blank)
+    if (!lines[lines.length - 1]) {
+        lines.pop();
+    }
+
+    var colNames = lines.shift().replace(/"/g, '').split(';');
+
+    const fieldConversions = {
+        Length: function(value) { return parseFloat(value) / 1000 },
+        StartTime: function(value) { return parseFloat(value) / 1000 },
+        StreamStart: function(value) { return parseFloat(value) / 1000 },
+        PlayRate: function(value) { return parseFloat(value) },
+        FadeTimeIn: function(value) { return parseFloat(value) / 1000 },
+        FadeTimeOut: function(value) { return parseFloat(value) / 1000 },
+    };
+
+    var edl = []; // Array of clips (aka "reels")
+    for (var rowIndex = 0; rowIndex < lines.length; rowIndex++) { 
+        var cols = lines[rowIndex].replace(/"/g, '').split('; ');
+        var row = {};
+        for (var colIndex = 0; colIndex < cols.length; colIndex++) { 
+            var fieldName = colNames[colIndex];
+            var value = cols[colIndex];
+            if (fieldConversions[fieldName]) {
+                value = fieldConversions[fieldName](value);
+            }
+            row[fieldName] = value;
+        }
+        edl.push(row);
+    }
+
+    return edl;
+}
+
 function main(options) {
     var edlFile;
     edlFile = File.openDialog('Select an EDL file');
@@ -6,53 +183,11 @@ function main(options) {
         return;
     }
 
-    function parseEDL(edlFile) {
-        edlFile.open('r');
-        var lines = edlFile.read().split('\n');
-        edlFile.close();
-
-        // Remove last row (expected to be blank)
-        lines.pop(); // TODO: Check whether last row is blank
-
-        var colNames = lines.shift().replace(/"/g, '').split(';');
-
-        var edl = []; // Array of clips (aka "reels")
-        for (var rowIndex = 0; rowIndex < lines.length; rowIndex++) { 
-            var cols = lines[rowIndex].replace(/"/g, '').split('; ');
-            edl.push({});
-            for (var colIndex = 0; colIndex < cols.length; colIndex++) { 
-                var field = colNames[colIndex];
-                edl[rowIndex][field] = cols[colIndex];
-                continue;
-            }
-        }
-
-        // perhaps have a dictionary of field names and functions to both cast and convert (e.g secs to ms)
-        var floatFields = [
-            'Length',
-            'StartTime', 
-            'StreamStart',
-            'StreamLength',
-            'PlayRate',
-            'PlayPitch',
-            'FadeTimeIn',
-            'FadeTimeOut'
-        ];
-
-        for (var i = 0; i < floatFields.length; i++) {
-            for (var j = 0; j < edl.length; j++) {
-                edl[j][floatFields[i]] = parseFloat(edl[j][floatFields[i]]);
-            }
-        }
-
-        return edl;
-    }
-
     var edl = parseEDL(edlFile);
-    if (options['reverseLayerOrder'])
+     if (options['reverseLayerOrder'])
         edl.reverse();
 
-    var compBaseDuration = 10; // Subtracted later // Default for now, must be calculated
+    var compBaseDuration = 1; // Subtracted later
     var comp = app.project.items.addComp(
         edlFile.name.split('.txt')[0],
         options.compWidth,
@@ -63,186 +198,46 @@ function main(options) {
     );
 
     var failedImports = {};
-    var ignoreFailedImports = false;
-
-    function getPlaceholder(edlClip) {
-        return app.project.importPlaceholder(
-            edlClip.FileName,
-            options['compWidth'],
-            options['compHeight'],
-            options['compFrameRate'],
-            edlClip.StreamLength / 1000
-        )
-    }
-
-    function getSolid(edlClip, solidColorHex) {
-        edlClip.FileName = 'Solid';
-        var footageItem = getPlaceholder(edlClip);
-        function hexToFloats(hex) {
-            hex = hex.replace(/^#/, '');
-            if (!(hex.length !== 3 || hex.length !== 6))
-                alert('Invalid hex code!');
-            if (hex.length === 3) // Expand shorthand (e.g., #abc -> #aabbcc)
-              hex = hex.split('').map(function(c) {return c + c}).join('');
-            var r = parseInt(hex.substring(0, 2), 16) / 255;
-            var g = parseInt(hex.substring(2, 4), 16) / 255;
-            var b = parseInt(hex.substring(4, 6), 16) / 255;
-            return [r, g, b];
-        }
-        footageItem.replaceWithSolid(
-            hexToFloats(solidColorHex),
-            'Solid',
-            options['compWidth'],
-            options['compHeight'],
-            options['compPixelAspect']
-        );
-        return footageItem;
-    }
-
-    function findFootageItem(edlClip, clipFile) {
-        // Project items are indexed from 1
-        for (var j = 1; j <= app.project.numItems; j++) {
-            var projectItem = app.project.items[j];
-            // Solids have no FileName
-            if (!edlClip.FileName && projectItem instanceof SolidSource)
-                return projectItem;
-            else if (projectItem.file && projectItem.file.fullName === clipFile.fullName)
-                return projectItem;
-        }
-    }
-
-    function importFootage(edlClip) {
-        var clipFile = File(edlClip.FileName);
-        var footageItem = findFootageItem(edlClip, clipFile);
-
-        if (footageItem)
-            return footageItem;
-
-        if (!edlClip.FileName)
-            return getSolid(edlClip, options['solidColorHex']);
-        else if (clipFile.exists)
-            return app.project.importFile(new ImportOptions(clipFile));
-        else {
-            if (!ignoreFailedImports) {
-                ignoreFailedImports = Window.confirm(
-                    'Failed to import file:\n' + edlClip.FileName +
-                    '\nA placeholder will be used instead.' +
-                    '\n\nPress OK to suppress this warning.'
-                );
-            }
-            return getPlaceholder(edlClip);
-        }
-    }
 
     for (var clipIndex = 0; clipIndex < edl.length; clipIndex++) {
         var clip = edl[clipIndex];
         if (failedImports[clip.FileName])
             continue;
-        var footageItem = importFootage(clip);
+        var footageItem = importFootage(clip, options);
         var layer = comp.layers.add(footageItem);
 
         // Stretch layer first (if necessary)
         layer.stretch = 100 / clip.PlayRate;
 
         // Trim points (snapped to the closest frame)
-        layer.inPoint = Math.round((clip.StreamStart / 1000) / comp.frameDuration) * comp.frameDuration;
-        layer.outPoint = Math.round((clip.StreamStart / 1000 + clip.Length / 1000) / comp.frameDuration) * comp.frameDuration; 
+        layer.inPoint = Math.round((clip.StreamStart) / comp.frameDuration) * comp.frameDuration;
+        layer.outPoint = Math.round((clip.StreamStart + clip.Length) / comp.frameDuration) * comp.frameDuration; 
 
         // Timeline position
-        layer.startTime = (clip.StartTime / 1000) - layer.inPoint;
+        layer.startTime = (clip.StartTime) - layer.inPoint;
     
         // Layer color
         layer.label = (clipIndex + 1) % 16; 
 
-        function applyFades(clip, layer, propHandle) {
-            function getKeyframeEase(curveType) {
-                var easeIn;
-                var easeOut;
-
-                if (curveType == 1) { // Linear
-                    return null;
-                } else if (curveType == -2) { // Slow in, fast out
-                    easeIn = new KeyframeEase(0, 75);
-                    easeOut = new KeyframeEase(0, 0.1);
-                } else if (curveType == 2) { // Fast in, slow out
-                    easeIn = new KeyframeEase(0, 0.1);
-                    easeOut = new KeyframeEase(0, 75);
-                } else if (curveType == 4) { // Easy Ease
-                    easeIn = new KeyframeEase(0, 33.33);
-                    easeOut = easeIn;
-                } else if (curveType == -4) { // Inverted Easy Ease                    
-                    easeIn = new KeyframeEase(100, 5);
-                    easeOut = easeIn;
-                } else {
-                    alert('Unregistered CurveType: ' + curveType);
-                }
-
-                return easeIn && easeOut ? [easeIn, easeOut] : null;
-            }
-
-            var minValue = 0;
-            var maxValue = 100;
-
-            if (clip.FadeTimeIn > 0) {
-                var fadeStart = layer.inPoint;
-                var fadeEnd = fadeStart + (clip.FadeTimeIn / 1000);
-                propHandle.setValueAtTime(fadeStart, minValue);
-                propHandle.setValueAtTime(fadeEnd, maxValue);
-                
-                var easeArgs = getKeyframeEase(clip.CurveIn);
-                if (easeArgs) {
-                    propHandle.setTemporalEaseAtKey(1, [easeArgs[0]]);
-                    propHandle.setTemporalEaseAtKey(2, [easeArgs[1]]);
-                }
-
-                if (options['markKeyframes']) { 
-                    var marker = new MarkerValue("Fade In");
-                    marker.duration = fadeEnd - fadeStart;
-                    layer.marker.setValueAtTime(fadeStart, marker);
-                }
-            }
-
-            if (clip.FadeTimeOut > 0) {
-                var fadeStart = layer.outPoint - (clip.FadeTimeOut / 1000);
-                var fadeEnd = layer.outPoint;
-                
-                propHandle.setValueAtTime(fadeStart, maxValue);
-                propHandle.setValueAtTime(fadeEnd, minValue);
-
-                var easeArgs = getKeyframeEase(clip.CurveOut);
-                if (easeArgs) {
-                    propHandle.setTemporalEaseAtKey(propHandle.numKeys - 1, [easeArgs[0]]);
-                    propHandle.setTemporalEaseAtKey(propHandle.numKeys, [easeArgs[1]]);
-                };
-
-                if (options['markKeyframes']) {
-                    var marker = new MarkerValue("Fade Out");
-                    marker.duration = fadeEnd - fadeStart;
-                    layer.marker.setValueAtTime(fadeStart, marker);
-                }
-            }
-        }
-
         var mediaType = clip.MediaType.toLowerCase();
+        var markKeyframes = options['markKeyframes'];
         if (mediaType === 'audio') { 
             layer.enabled = false; // Disables video
             var mixer = layer.Effects.addProperty('Stereo Mixer');
-            applyFades(clip, layer, mixer['Left Level']); 
-            applyFades(clip, layer, mixer['Right Level']);
+            applyFades(clip, layer, mixer['Left Level'], markKeyframes);
+            applyFades(clip, layer, mixer['Right Level'], markKeyframes);
         } else if (mediaType === 'video') {
             layer.audioEnabled = false; // Disables audio
-            applyFades(clip, layer, layer['opacity']);  
+            applyFades(clip, layer, layer['opacity'], markKeyframes);
         } else {
             alert('Media type not supported: ' + clip.MediaType); 
         }
 
-        var clipDuration = layer.outPoint - layer.inPoint;
-        comp.duration += clipDuration;
+        comp.duration += layer.outPoint - layer.inPoint;
     }
 
-    // comp.duration -= compBaseDuration;
+    comp.duration -= compBaseDuration;
     comp.openInViewer();
-    return comp;
 }
 
 function drawPanel(rootPanel) {
@@ -279,20 +274,20 @@ function drawPanel(rootPanel) {
     var chkMarkKeyframes = grpOptions.add('checkbox', undefined, 'Mark Keyframes');
     var chkReverseLayerOrder = grpOptions.add('checkbox', undefined, 'Reverse Layer Order');
     
-    var comp;
     panel.add('button', undefined, 'Import EDL...').onClick = function() {
-        var options = {
-            compWidth: parseInt(txtCompWidth.text),
-            compHeight: parseInt(txtCompHeight.text),
-            compFrameRate: parseInt(txtCompFrameRate.text),
-            compPixelAspect: 1,
-            solidColorHex: txtSolidColorHex.text,
-            markKeyframes: chkMarkKeyframes.value,
-            reverseLayerOrder: chkReverseLayerOrder.value
-        }
-        comp = main(options);
-        // panel.close(); // If running undocked
     };
+    var options = {
+        compWidth: parseInt(txtCompWidth.text),
+        compHeight: parseInt(txtCompHeight.text),
+        compFrameRate: parseInt(txtCompFrameRate.text),
+        compPixelAspect: 1,
+        ignoreFailedImports: false,
+        solidColorHex: txtSolidColorHex.text,
+        markKeyframes: chkMarkKeyframes.value,
+        reverseLayerOrder: chkReverseLayerOrder.value
+    }
+    main(options);
+    // panel.close(); // If running undocked
 
     return panel;
 }
@@ -311,4 +306,4 @@ if (panel instanceof Window) {
 
 // mirror audio gain (at `if (mediaType === 'audio')`)
 // if in/out points are the same, merge both layers into one
-// fix comp duration, remove double quotes & those if/elses
+// fix comp duration
